f074e109f4404b796c15079d96b2039b08c6f000
Byungin Kim <65350890+byunginK@users.noreply.github.com>
2020-05-28 20:01:22 +0900

    Update JAVA BaseballGame.java

--- JAVA BaseballGame.java
+++ JAVA BaseballGame.java
@@ -20,88 +20,105 @@ public class MainClass {
 // messege 출력  못 마추면 uset 인풋으로 다시 돌아가고 기회는 10번
 // end  replay 여부 확인
 		
-		Scanner sc = new Scanner(System.in);
-		
 		int r_num[] = new int[3];
 		int u_num[] = new int[3];
 		boolean clear = false;
 		
-		boolean swit[] = new boolean[10];			//랜덤 숫자의 개수의 맞게 설정
-		
+		random(r_num); 		// 랜덤넘버 생성
+		int w = 0;
+		while(w < 10) {
+			userInput(u_num);
+			boolean b = finding(u_num, r_num);
+			if(b == true) {
+				clear = true;
+				break;
+			}
+			w++;
+		}	
+		resultPrint(clear);
+}
+// ---------------------------------------------------------메인 함수 끝 메서드 시작
 
+static void random(int r_num[]) {					//목적: 배열에 랜덤숫자 3개 대입
+		boolean swit[] = new boolean[10];			// 랜덤 숫자 중복 방지		
 		for (int i = 0; i < swit.length; i++) {
-			swit[i] = false;    				// 00000 00000  전부 false로 우선 설정( 중복방지 코드 참고)
+			swit[i] = false;    
 		}
-		
-		int r, w; 			// 아래 중복 방지 코드
+		int r, w; 			
 		w =0;
 		while(w<3) {
-			r = (int)(Math.random()*10);			// r에 랜덤 숫자를 넣어준다.  나올 숫자도 맞게 설정
+			r = (int)(Math.random()*10);		
 			if(swit[r] ==false) {				
 				swit[r] = true;
-				r_num[w] = r + 1;			// 0 부터 랜덤 숫자가 들어가기 때문에  +1을 해준다.
+				r_num[w] = r + 1;			.// 중복되지 않은 랜덤숫자 r_num배열에 대입
 				w++;
 			}	
 		}
-		w = 0;
-		int strike, ball;
-		while(w<10) {
-			// user input  u1 != u2 != u3
-			int w1;
-			while(true) {
-				boolean check = false;
-				w1 = 0;
-				while(w1 < 3) {
-					System.out.println((w1 + 1)+"번째 수 = ");
-					u_num[w1] = sc.nextInt();
-					w1++;
-				}
+		for (int i = 0; i < r_num.length; i++) {
+			System.out.println(i+" : "+r_num[i]);
+		}
+	}
+
+static void userInput(int u_num[]) {
+		Scanner sc = new Scanner(System.in);
+		boolean check;
+		int w1;
+	
+		while(true) {
+			check = false;	
+			w1 = 0;
+			while(w1 < 3) {
+				System.out.println((w1 + 1)+"번째 수 = ");
+				u_num[w1] = sc.nextInt();
+				w1++;
+			}
 				
 				//같은 숫자가 있는지 체크
-			out: for (int i = 0; i < u_num.length; i++) {
+			out:for (int i = 0; i < u_num.length; i++) {
 					for (int j = 0; j < u_num.length; j++) {
 						if(u_num[i] == u_num[j] && i!= j) {
-							check = true;   //입력한 같은 숫가가 있음
+							check = true;   //입력한 같은 숫자가 있음
 							break out;
 						}
 					}
 				}
-				if(check == false) {
-					break;
-				}
-				System.out.println("입력한 숫자중에 중복되는 숫자가 있습니다. 다시 입력해 주십시오.");
-			}
-			
-			// process(비교)
-			strike = ball = 0;
-				// ball
-			for (int i = 0; i < u_num.length; i++) {
-				for (int j = 0; j < r_num.length; j++) {
-					if(u_num[i] == r_num[j] && i !=j) {
-						ball++;
-					}
-				}
-			}
-				// strike
-			for (int i = 0; i < u_num.length; i++) {
-				if(u_num[i]==r_num[i]) {
-					strike++;
-				}
-			}
-				// 탈출
-			if(strike > 2) {
-				clear = true;
+			if(check == false) {
 				break;
 			}
-			//메세지 출력
-			System.out.println(strike+"스트라이크"+ball + "볼 입니다.");
-			w++;
+			System.out.println("입력한 숫자중에 중복되는 숫자가 있습니다. 다시 입력해 주십시오.");
 		}
-		if(clear) {
-			System.out.println("Game Clear");
-		}else {
-			System.out.println("Game Over");
-		}
-	}
+}		
 
+static boolean finding(int u_num[], int r_num[]) {
+		int strike, ball;
+		strike = ball = 0;
+			// ball
+		for (int i = 0; i < u_num.length; i++) {
+			for (int j = 0; j < r_num.length; j++) {
+				if(u_num[i] == r_num[j] && i !=j) {
+					ball++;
+				}
+			}
+		}
+			// strike
+		for (int i = 0; i < u_num.length; i++) {
+			if(u_num[i]==r_num[i]) {
+				strike++;
+			}
+		}
+			// 탈출
+		if(strike > 2) {
+			return true;
+		}
+		
+		System.out.println(strike+"스트라이크"+ball + "볼 입니다.");
+		return false;
+}
+
+static void resultPrint(boolean clear) {
+		if(clear) {
+			System.out.println("Game Clear!!");
+		}else {
+			System.out.println("Game Over~ ");
+		}	
 }
