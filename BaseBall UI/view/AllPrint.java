1af094c31e82b2ea52a4b4de267ab25a79e72216
Byungin Kim <65350890+byunginK@users.noreply.github.com>
2020-06-14 12:49:49 +0900

    Update AllPrint.java

--- BaseBall UI/view/AllPrint.java
+++ BaseBall UI/view/AllPrint.java
@@ -1,7 +1,30 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+
+import Panel.AllPrintPanel;
 
 public class AllPrint extends JFrame {
 
+	Container contentPane;
+	
+	public AllPrint() {
+		
+		setLayout(null);
+		
+		AllPrintPanel ap = new AllPrintPanel(this);
+		
+		contentPane = getContentPane();
+		contentPane.add(ap);
+		contentPane.setLocation(100, 100);
+		
+		setBounds(0, 0, 640, 500);
+		setVisible(true);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
 }
